c16aca65925e51653456539929764367208b3dfb
Stuart McKay <stuart51994@gmail.com>
2019-10-10 17:58:08 -0700

    declare elasticsearch service

---
+++ data.js
@@ -0,0 +1,18 @@
+//data.js
+//require the Elasticsearch librray
+const elasticsearch = require('elasticsearch');
+// instantiate an Elasticsearch client
+const client = new elasticsearch.Client({
+   hosts: [ 'http://localhost:9200']
+});
+// ping the client to be sure Elasticsearch is up
+client.ping({
+     requestTimeout: 30000,
+ }, function(error) {
+ // at this point, eastic search is down, please check your Elasticsearch service
+     if (error) {
+         console.error('Elasticsearch cluster is down!');
+     } else {
+         console.log('Everything is ok');
+     }
+ });
