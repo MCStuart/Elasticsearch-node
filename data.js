7a77f1b6706a71611b1245f414d5200c9b8e21e9
Stuart McKay <stuart51994@gmail.com>
2019-10-10 18:40:32 -0700

    add cities.json for search index

--- data.js
+++ data.js
@@ -19,11 +19,25 @@ client.ping({
 
 // create a new index called scotch.io-tutorial. If the index has already been created, this function fails safely
 client.indices.create({
-      index: 'scotch.io-tutorial'
-  }, function(error, response, status) {
-      if (error) {
-          console.log(error);
-      } else {
-          console.log("created a new index", response);
-      }
+  index: 'scotch.io-tutorial'
+}, function (error, response, status) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("created a new index", response);
+  }
+});
+
+// add data to the indec that has already been created
+client.index({
+  index: 'scotch.io-tutorial',
+  id: '1',
+  type: 'cities_list',
+  body: {
+    "Key1": "Content for key one",
+    "Key2": "Content for key two",
+    "Key3": "Content for key three",
+  }
+}, function (err, resp, status) {
+  console.log(resp);
 });
