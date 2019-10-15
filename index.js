65685a66d203e66ef2228f3634e61e65106a313a
Stuart McKay <stuart51994@gmail.com>
2019-10-14 17:05:38 -0700

    add a new route for template2.html, fix formatting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const elasticsearch = require('elasticsearch');
 // instantiate an elasticsearch client
 const client = new elasticsearch.Client({
-   hosts: [ 'http://localhost:9200']
+  hosts: ['http://localhost:9200']
 });
 //require Express
 const express = require('express');
@@ -16,15 +16,15 @@ const path = require('path');
 
 // ping the client to be sure Elasticsearch is up
 client.ping({
-     requestTimeout: 30000,
- }, function(error) {
- // at this point, eastic search is down, please check your Elasticsearch service
+  requestTimeout: 30000,
+}, function (error) {
+  // at this point, eastic search is down, please check your Elasticsearch service
   if (error) {
     console.error('elasticsearch cluster is down!');
   } else {
     console.log('Everything is ok');
   }
- });
+});
 
 
 // use the bodyparser as a middleware  
@@ -32,9 +32,9 @@ app.use(bodyParser.json())
 // set port for the app to listen on
 app.set('port', process.env.PORT || 3001);
 // set path to serve static files
-app.use(express.static( path.join( __dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 // enable CORS 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -42,37 +42,44 @@ app.use(function(req, res, next) {
 });
 
 // defined the base route and return with an HTML file called tempate.html
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile('template.html', {
-     root: path.join( __dirname, 'views' )
-   });
+    root: path.join(__dirname, 'views')
+  });
 })
 
 // define the /search route that should return elastic search results 
-app.get('/search', function (req, res){
+app.get('/search', function (req, res) {
   // declare the query object to search elastic search and return only 200 results from the first result found. 
   // also match any data where the name is like the query string sent in
   let body = {
     size: 200,
-    from: 0, 
+    from: 0,
     query: {
       match: {
-          name: req.query['q']
+        name: req.query['q']
       }
     }
   }
   // perform the actual search passing in the index, the search query and the type
-  client.search({index:'scotch.io-tutorial',  body:body, type:'cities_list'})
-  .then(results => {
-    res.send(results.hits.hits);
-  })
-  .catch(err=>{
-    console.log(err)
-    res.send([]);
-  });
+  client.search({ index: 'scotch.io-tutorial', body: body, type: 'cities_list' })
+    .then(results => {
+      res.send(results.hits.hits);
+    })
+    .catch(err => {
+      console.log(err)
+      res.send([]);
+    });
 
 })
 // listen on the specified port
-app .listen( app.get('port'), function(){
+app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
-} );
+});
+
+// decare a new route. This route serves a static HTML template called template2.html
+app.get('/v2', function (req, res) {
+  res.sendFile('template2.html', {
+    root: path.join(__dirname, 'views')
+  });
+})
